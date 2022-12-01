b6dc875cb9dde17c3ed04cd94b8717d36c579f55
Alexey <105908089+nomadpyn@users.noreply.github.com>
2022-12-01 11:43:43 +0700

    Commit 3
    create class Func for static methods to help in program

---
+++ Dictionary_CSharp/Func.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary_CSharp
+{
+    public class Func
+    {
+        static public string getWord()
+        {
+            string word;
+            do
+            {
+                word = Console.ReadLine();
+
+                if (word == "")
+                {
+                    Console.WriteLine("Вы не ввели слово");
+                }
+            }
+            while (word == "");
+
+            return word;
+        }
+    }
+}

d2b411e28d9f3bd634772939741ed4ffd56e0f73
Alexey <105908089+nomadpyn@users.noreply.github.com>
2022-12-02 11:49:26 +0700

    Commit 19
    class Menu
    add Level2

--- Dictionary_CSharp/Menu.cs
+++ Dictionary_CSharp/Menu.cs
@@ -83,7 +83,50 @@ namespace Dictionary_CSharp
             while (choise.Key != ConsoleKey.D1 && choise.Key != ConsoleKey.D0);
 
         }
- 
+        static void Level2(ref Dctnr obj)
+        {
+            ConsoleKeyInfo choise;
+            Console.WriteLine(obj);
+            do
+            {
+                Console.WriteLine("1 - Показать весь словарь, 2 - Искать слово, 3 - Операции со словарем, 0 - В начало");
+                choise = Console.ReadKey();
+                Console.Clear();
+                switch (choise.Key)
+                {
+                    case ConsoleKey.D1:
+                        {
+                            obj.showDctnr();
+                            if (Menu.ContinueWork() == false)
+                                Menu.Level();
+                            break;
+                        }
+                    case ConsoleKey.D2:
+                        {
+                            obj.searchWord();
+                            if (Menu.ContinueWork() == false)
+                                Menu.Level();
+                            break;
+                        }
+                    case ConsoleKey.D3:
+                        {
+                            Menu.Level3();
+                            break;
+                        }
+                    case ConsoleKey.D0:
+                        {
+                            Menu.Level();
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Вы не сделали выбор");
+                            break;
+                        }
+                }
+
+            }
+            while (choise.Key != ConsoleKey.D0);
         }
         static bool ContinueWork()
         {
